5cb03b5546ae99f6213917726fd70b870ecbdb3d
bs20242 <bsimao11@gmail.com>
2025-05-07 23:08:15 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,3 +1,16 @@
-from c_lib import somar
+from c_lib import somar, subtrair, multiplicar, dividir
 
-print(somar(5, 3))  # Deve imprimir: 8
+print("Testando a Calculadora C_lib:\n")
+
+# Soma
+print("Soma 5 + 3 =", somar(5, 5))  
+
+# Subtração
+print("Subtração 10 - 4 =", subtrair(16, 4))  
+
+# Multiplicação
+print("Multiplicação 7 * 6 =", multiplicar(7, 3))  
+
+# Divisão
+print("Divisão 20 / 5 =", dividir(20, 5)) 
+
